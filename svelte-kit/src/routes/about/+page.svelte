f38e8b2fd9b15dce367e91d81f2ee4ac9f3cb420
Lynnsanity <lynnellesaavedra8@gmail.com>
2023-09-05 00:28:37 -0500

    more about me

--- svelte-kit/src/routes/about/+page.svelte
+++ svelte-kit/src/routes/about/+page.svelte
@@ -8,26 +8,39 @@
             <div class="banner">
                 <img src="setup.jpg" alt="setup" class="rounded-md border border-pink-500">
             </div>
-            <div class="about-area p-8 flex flex-col md:flex-row bg-zinc-950 mt-4 md:mt-0 md:ml-4 flex-1 text-white">
+            <div class="p-8 flex flex-col md:flex-row bg-zinc-950 mt-4 md:mt-0 md:ml-4 flex-1 text-white">
                 <div class="description">
-                    <h1 class="text-2xl border-b-2 border-pink-500 pb-2 mb-4"> About Me </h1>
-                    <p>
-                        Hey! I'm Lynnelle. Right now, I'm employed at Wildcard Corp. as a Jr. DevOps Engineer/Systems Administrator. 
-                        My journey with the company started two years ago during my junior year at the University of Wisconsin - Stevens Point. 
-                        I began as a Web Developer intern, fell in love with Kubernetes and DevOps work, and then smoothly transitioned 
-                        into the roles I enjoy today.
-                        <br>
-                        <br> 
-                        When I'm not at work, you'll still find me in front of my computer, except I'd be playing PC games.
-                        Usually FPS, Battle Royales and MMORPGs catch my interest the most, with Overwatch 2 and Apex Legends being my
-                        current favorites. If I'm away from my computer, you can find me watching anime, strumming my guitar, and making 
-                        sure my dogs get their walks and playtime.
-                    </p>
+                    <h1 class="text-2xl border-b-2 border-pink-500 w-1/4 pb-2 mb-4"> About Me </h1>
+                        <p>
+                            Hey! I'm Lynnelle. Right now, I'm employed at Wildcard Corp. as a Jr. DevOps Engineer/Systems Administrator. 
+                            My journey with the company started two years ago during my junior year at the University of Wisconsin - Stevens Point. 
+                            I began as a Web Developer intern, fell in love with Kubernetes and DevOps work, and then smoothly transitioned 
+                            into the roles I enjoy today.
+                            <br>
+                            <br> 
+                            When I'm not at work, you'll still find me in front of my computer, except I'd be playing PC games.
+                            Usually FPS, Battle Royales and MMORPGs catch my interest the most, with Overwatch 2 and Apex Legends being my
+                            current favorites. If I'm away from my computer, you can find me watching anime, strumming my guitar, and making 
+                            sure my dogs get their walks and playtime.
+                            <br>
+                            <br>
+                            <em>"Tutturu!" -Mayuri Shiina </em>  
+                        </p>
                 </div>
                 <div class="me-pic mx-auto p-10">
                     <img src="graduation.jpg" alt="lynn desu" class="rounded-md border-2 border-pink-500 max-w-xs">
                 </div>
-            </div>   
+                
+            </div>
+            <div class="p-8 flex flex-col md:flex-row bg-zinc-950 mt-4 md:mt-0 md:ml-4 flex-1 text-white">
+                <div class="description">
+                    <h1 class="text-2xl border-b-2 border-pink-500 w-1/2 pb-2 mb-4">Projects</h1>
+                        <p>
+                            Want to see some things I've worked on? Check out my projects page! 
+                            <br>
+                        </p>
+                </div>
+            </div> 
         </div>
     </body>
 </main>
@@ -43,11 +56,6 @@
     /* font color of my about description */
     .description {
         color: white;
-    }
-
-    /* line under "About Me" heading */
-    .description h1 {
-        width: 35%;
     }
 
     /* the banner image size */
