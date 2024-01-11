5ee50b7a47088112fda52e9c578a7f711f204ca1
Lynnelle Saavedra <94505413+Lynnsanity@users.noreply.github.com>
2024-01-11 10:08:07 -0600

    navbar item background gross

--- svelte-kit/src/lib/components/+navbar.svelte
+++ svelte-kit/src/lib/components/+navbar.svelte
@@ -10,7 +10,7 @@
     });
   });
 </script>
-<nav class="bg-transparent border-gray-200 light:bg-transparent dark:bg-transparent">
+<nav class="bg-transparent light:bg-transparent dark:bg-transparent">
     <!-- left side of navbar, lynnsanity logo-->
     <div class="max-w-screen-xl flex flex-wrap items-center mx-auto justify-between p-4">
       <a href="/" class="flex items-center">
@@ -25,18 +25,18 @@
       </button>
       <!-- links to click to -->
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul class="text-white bg-transparent font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-12 md:mt-0 md:border-0 dark:bg-gray-800 dark:border-gray-700">
+        <ul class="text-white bg-transparent font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-12 md:mt-0 md:border-0 light:bg-transparent dark:bg-transparent">
           <li>
-            <a href="/about" class="text-white bg-transparent block py-2 pl-3 pr-4 rounded hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 light:text-white dark:text-white">About</a>
+            <a href="/about" class="text-white bg-transparent block py-2 pl-3 pr-4 rounded hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 light:text-white dark:text-white">About</a>
           </li>
           <li>
-            <a href="/projects" class="text-white bg-transparent block py-2 pl-3 pr-4 rounded hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 light:text-white dark:text-white">Projects</a>
+            <a href="/projects" class="text-white bg-transparent block py-2 pl-3 pr-4 rounded hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 light:text-white dark:text-white">Projects</a>
           </li>
           <li>
-            <a href="/resume" class="text-white bg-transparent block py-2 pl-3 pr-4 rounded hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 light:text-white dark:text-white">Resume</a>
+            <a href="/resume" class="text-white bg-transparent block py-2 pl-3 pr-4 rounded hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 light:text-white dark:text-white">Resume</a>
           </li>
           <li>
-            <a href="/contact" class="text-white bg-transparent block py-2 pl-3 pr-4 rounded hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 light:text-white dark:text-white">Contact</a>
+            <a href="/contact" class="text-white bg-transparent block py-2 pl-3 pr-4 rounded hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 light:text-white dark:text-white">Contact</a>
           </li>
         </ul>
       </div>
